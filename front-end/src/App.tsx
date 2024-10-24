555d40e6ea1e2544735d418fa0004dcb9fa7e3eb
Mwero Abdalla <mwerothewebmaker@gmail.com>
2024-10-24 20:52:28 +0300

    feat : add todo delete feature

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -1,49 +1,66 @@
 import { useEffect, useState } from "react";
 import { TodoItem } from "./components/TodoItem";
 import { AddTodo } from "./components/AddTodo";
-const BASE_URL="http://localhost:5000"
+const BASE_URL = "http://localhost:5000";
 const headers = {
-  "Content-Type":"application/json"
-}
+  "Content-Type": "application/json",
+};
 export default function App() {
   const [todos, setTodos] = useState([]);
-  
+
   useEffect(() => {
+    getTodos();
+  }, []);
+
+  //handle getTodo
+  const getTodos = async () => {
+   
     fetch(`${BASE_URL}/todos`)
       .then((res) => res.json())
       .then((data) => setTodos(data))
       .then((err: any) => console.log(err));
-  }, []);
+  };
 
+//hadnle delete todo
+const handleDeleteTodo = async (id:any) => {
+  try {
+    const res = await fetch(`${BASE_URL}/todos/${id}`, {
+      method: "DELETE",
+         headers,
+    });
+    if (!res.ok) throw Error("Error deleting the todo");
+    alert("Todo deleted successfully!");
+    //get all todos
+    await getTodos();
+  } catch (error) {
+    alert("Todo creation failed!");
+  }
+};
 
-// handleAddTodo
-const handleAddTodo = async(data:any)=>{
-try {
-const res =   await  fetch(`${BASE_URL}/todos`, {
-    method:"POST",
-    body:JSON.stringify(data),
-    headers
-
-  })
-if(!res.ok) throw Error("Error creating the todo")
-  alert('Todo created successfully!')
-
-} catch (error) {
-  alert('Todo creation failed!')
-
-}
-}
-
-
-
+  // handleAddTodo
+  const handleAddTodo = async (data: any) => {
+    try {
+      const res = await fetch(`${BASE_URL}/todos`, {
+        method: "POST",
+        body: JSON.stringify(data),
+        headers,
+      });
+      if (!res.ok) throw Error("Error creating the todo");
+      alert("Todo created successfully!");
+      //get all todos
+      await getTodos();
+    } catch (error) {
+      alert("Todo creation failed!");
+    }
+  };
 
   if (!todos.length) return <div>Loading ....</div>;
 
   return (
     <div className="max-w-md mx-auto shadow-md bg-slate-200 px-2 py-5 my-6">
-      <AddTodo  handleAddTodo = {handleAddTodo} />
+      <AddTodo handleAddTodo={handleAddTodo} />
       {todos.map((todo: any) => (
-        <TodoItem key={todo._id} {...todo} />
+        <TodoItem key={todo._id} {...todo} delete={handleDeleteTodo} update={()=>null} />
       ))}
     </div>
   );
--- front-end/src/components/TodoItem.tsx
+++ front-end/src/components/TodoItem.tsx
@@ -1,22 +1,39 @@
-import { Button } from "./Button"
+import { Button } from "./Button";
 
-interface TodoItemProps{
-    _id:string
-    title:string,
-    completed:boolean
+interface TodoItemProps {
+  _id: string;
+  title: string;
+  completed: boolean;
+  delete: (id: any) => Promise<void>;
+  update: (id: any) => Promise<void>;
 }
 
-export function TodoItem({_id,title, completed}:TodoItemProps) {
-console.log({_id, completed})
-
+export function TodoItem({
+  _id,
+  title,
+  completed,
+  delete: handleDelete,
+  update,
+}: TodoItemProps) {
+  console.log(completed)
   return (
     <div className="flex items-center justify-between shadow mb-2 ">
-        <h3 className="first-letter:uppercase p-2 text-lg">{title}</h3>
+      <h3 className="first-letter:uppercase p-2 text-lg">{title}</h3>
 
-        <div className="w-fit">
-            <Button className="bg-orange-600 py-2 px-4 text-sm">Update</Button>
-            <Button className="bg-red-600 py-2 px-4 text-sm">Delete</Button>
-        </div>
+      <div className="w-fit">
+        <Button
+          className="bg-orange-600 py-2 px-4 text-sm"
+          onClick={() => update(_id)}
+        >
+          Update
+        </Button>
+        <Button
+          className="bg-red-600 py-2 px-4 text-sm"
+          onClick={() => handleDelete(_id)}
+        >
+          Delete
+        </Button>
+      </div>
     </div>
-  )
+  );
 }
